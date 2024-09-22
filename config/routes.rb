a6ab28ea09c80cf32e1b869c3eb1882def540df5
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2024-09-22 12:42:42 +0200

    Added all the CRUD actions without any buttons on the page to navigate between the crud pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :articles
+  root to: 'articles#index'
+  # get 'articles', to: 'articles#index'
+  # get 'articles/new', to: 'articles#new'
+  # post 'articles', to: 'articles#create'
+  # get 'articles/:id/edit', to: 'articles#edit'
+  # get 'articles/:id', to: 'articles#show'
+  # patch 'articles/:id', to: 'articles#update'
+  # delete 'articles/:id', to: 'articles#destroy'
 end
