9dadee81654ac3521322e3a7e004f90ffeadec4d
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2024-09-22 11:59:21 +0200

    Created a seed file to upload false data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+# puts 'Creating 10 fake Articles...'
+10.times do
+  article = Article.new(
+    title: Faker::Lorem.sentence(word_count: 3),
+    content: Faker::Lorem.paragraph
+  )
+  article.save!
+end
+# puts 'Finished!'
